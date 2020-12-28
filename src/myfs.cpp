45447fafaf5be7f86300381beaa029ea70a76b5b
Felix Schneider <fs@felix-schneider.org>
2020-12-28 12:31:25 +0100

    Delete trailing spaced

--- src/myfs.cpp
+++ src/myfs.cpp
@@ -13,7 +13,6 @@
 
 #undef DEBUG
 
-// TODO: Comment lines to reduce debug messages
 #define DEBUG
 #define DEBUG_METHODS
 #define DEBUG_RETURN_VALUES
@@ -28,192 +27,225 @@
 #include "myfs-info.h"
 #include "blockdevice.h"
 
-// TODO: [PART 2] You may move some helper messages here
-
 // DO NOT EDIT ANYTHING BELOW THIS LINE!!!
 
-MyFS::MyFS() {
-    this->logFile= stderr;
+MyFS::MyFS()
+{
+    this->logFile = stderr;
 }
 
-MyFS::~MyFS() {
-
+MyFS::~MyFS()
+{
 }
 
-MyFS* MyFS::_instance = NULL;
+MyFS *MyFS::_instance = NULL;
 
-MyFS* MyFS::Instance() {
-    if(_instance == NULL) {
+MyFS *MyFS::Instance()
+{
+    if (_instance == NULL)
+    {
         fprintf(stderr, "ERROR: MyFS Instance not set!");
         exit(-1);
     }
     return _instance;
 }
 
-int MyFS::fuseGetattr(const char *path, struct stat *statbuf) {
+int MyFS::fuseGetattr(const char *path, struct stat *statbuf)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseMknod(const char *path, mode_t mode, dev_t dev) {
+int MyFS::fuseMknod(const char *path, mode_t mode, dev_t dev)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseUnlink(const char *path) {
+int MyFS::fuseUnlink(const char *path)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseRename(const char *path, const char *newpath) {
+int MyFS::fuseRename(const char *path, const char *newpath)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseChmod(const char *path, mode_t mode) {
+int MyFS::fuseChmod(const char *path, mode_t mode)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseChown(const char *path, uid_t uid, gid_t gid) {
+int MyFS::fuseChown(const char *path, uid_t uid, gid_t gid)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseTruncate(const char *path, off_t newSize) {
+int MyFS::fuseTruncate(const char *path, off_t newSize)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseOpen(const char *path, struct fuse_file_info *fileInfo) {
+int MyFS::fuseOpen(const char *path, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseRead(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+int MyFS::fuseRead(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseWrite(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo) {
+int MyFS::fuseWrite(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseRelease(const char *path, struct fuse_file_info *fileInfo) {
+int MyFS::fuseRelease(const char *path, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseReaddir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo) {
+int MyFS::fuseReaddir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-void* MyFS::fuseInit(struct fuse_conn_info *conn) {
+void *MyFS::fuseInit(struct fuse_conn_info *conn)
+{
     RETURN(0);
 }
 
-int MyFS::fuseReadlink(const char *path, char *link, size_t size) {
+int MyFS::fuseReadlink(const char *path, char *link, size_t size)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseMkdir(const char *path, mode_t mode) {
+int MyFS::fuseMkdir(const char *path, mode_t mode)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseRmdir(const char *path) {
+int MyFS::fuseRmdir(const char *path)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseSymlink(const char *path, const char *link) {
+int MyFS::fuseSymlink(const char *path, const char *link)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseLink(const char *path, const char *newpath) {
+int MyFS::fuseLink(const char *path, const char *newpath)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseUtime(const char *path, struct utimbuf *ubuf) {
+int MyFS::fuseUtime(const char *path, struct utimbuf *ubuf)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseStatfs(const char *path, struct statvfs *statInfo) {
+int MyFS::fuseStatfs(const char *path, struct statvfs *statInfo)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseFlush(const char *path, struct fuse_file_info *fileInfo) {
+int MyFS::fuseFlush(const char *path, struct fuse_file_info *fileInfo)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseFsync(const char *path, int datasync, struct fuse_file_info *fi) {
+int MyFS::fuseFsync(const char *path, int datasync, struct fuse_file_info *fi)
+{
     LOGM();
     return 0;
 }
 
-int MyFS::fuseListxattr(const char *path, char *list, size_t size) {
+int MyFS::fuseListxattr(const char *path, char *list, size_t size)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseRemovexattr(const char *path, const char *name) {
+int MyFS::fuseRemovexattr(const char *path, const char *name)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseOpendir(const char *path, struct fuse_file_info *fileInfo) {
+int MyFS::fuseOpendir(const char *path, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseReleasedir(const char *path, struct fuse_file_info *fileInfo) {
+int MyFS::fuseReleasedir(const char *path, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseFsyncdir(const char *path, int datasync, struct fuse_file_info *fileInfo) {
+int MyFS::fuseFsyncdir(const char *path, int datasync, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseTruncate(const char *path, off_t offset, struct fuse_file_info *fileInfo) {
+int MyFS::fuseTruncate(const char *path, off_t offset, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-int MyFS::fuseCreate(const char *path, mode_t mode, struct fuse_file_info *fileInfo) {
+int MyFS::fuseCreate(const char *path, mode_t mode, struct fuse_file_info *fileInfo)
+{
     LOGM();
     RETURN(0);
 }
 
-void MyFS::fuseDestroy() {
+void MyFS::fuseDestroy()
+{
     LOGM();
 }
 
 #ifdef __APPLE__
-int MyFS::fuseSetxattr(const char *path, const char *name, const char *value, size_t size, int flags, uint32_t x) {
+int MyFS::fuseSetxattr(const char *path, const char *name, const char *value, size_t size, int flags, uint32_t x)
+{
 #else
-int MyFS::fuseSetxattr(const char *path, const char *name, const char *value, size_t size, int flags) {
+int MyFS::fuseSetxattr(const char *path, const char *name, const char *value, size_t size, int flags)
+{
 #endif
     LOGM();
     RETURN(0);
 }
-    
-#ifdef __APPLE__
-int MyFS::fuseGetxattr(const char *path, const char *name, char *value, size_t size, uint x) {
-#else
-int MyFS::fuseGetxattr(const char *path, const char *name, char *value, size_t size) {
-#endif
-    LOGM();
-    RETURN(0);
-}
-        
 
+#ifdef __APPLE__
+int MyFS::fuseGetxattr(const char *path, const char *name, char *value, size_t size, uint x)
+{
+#else
+int MyFS::fuseGetxattr(const char *path, const char *name, char *value, size_t size)
+{
+#endif
+    LOGM();
+    RETURN(0);
+}
