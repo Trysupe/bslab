e485bc0d0ce6125454165f26432124d08c486661
Felix Schneider <fs@felix-schneider.org>
2020-12-28 12:31:06 +0100

    Delete Todo

--- includes/macros.h
+++ includes/macros.h
@@ -9,38 +9,47 @@
 #ifndef macros_h
 #define macros_h
 
-#define error(str)                \
-do {                        \
-fprintf(stderr, str "\n");\
-exit(-1);\
-} while(0)
+#define error(str)                 \
+    do                             \
+    {                              \
+        fprintf(stderr, str "\n"); \
+        exit(-1);                  \
+    } while (0)
 
 #ifdef DEBUG
-#define LOGF(fmt, ...) \
-do { fprintf(this->logFile, "\t" fmt "\n", __VA_ARGS__); } while (0)
+#define LOGF(fmt, ...)                                      \
+    do                                                      \
+    {                                                       \
+        fprintf(this->logFile, "\t" fmt "\n", __VA_ARGS__); \
+    } while (0)
 
-#define LOG(text) \
-do { fprintf(this->logFile, "\t" text "\n"); } while (0)
+#define LOG(text)                               \
+    do                                          \
+    {                                           \
+        fprintf(this->logFile, "\t" text "\n"); \
+    } while (0)
 #else
 #define LOGF(fmt, ...)
 #define LOG(text)
 #endif
 
 #ifdef DEBUG_METHODS
-#define LOGM() \
-do { fprintf(this->logFile, "%s:%d:%s()\n", __FILE__, \
-__LINE__, __func__); } while (0)
+#define LOGM()                                           \
+    do                                                   \
+    {                                                    \
+        fprintf(this->logFile, "%s:%d:%s()\n", __FILE__, \
+                __LINE__, __func__);                     \
+    } while (0)
 #else
 #define LOGM()
 #endif
 
 #ifdef DEBUG_RETURN_VALUES
-#define RETURN(ret) \
-fprintf(this->logFile, "%s() returned %d\n", __func__, ret); return ret;
+#define RETURN(ret)                                              \
+    fprintf(this->logFile, "%s() returned %d\n", __func__, ret); \
+    return ret;
 #else
 #define RETURN(ret) return ret;
 #endif
-
-// TODO: Implement your own macros here!
 
 #endif /* macros_h */
