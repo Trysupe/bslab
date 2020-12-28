9df2477a8443716c616446c0031d1e0d9f461662
Felix Schneider <fs@felix-schneider.org>
2020-12-28 12:31:57 +0100

    Delete Todo

--- testing/tools.cpp
+++ testing/tools.cpp
@@ -9,15 +9,15 @@
 #include "catch.hpp"
 #include "tools.hpp"
 
-void gen_random(char *s, const int len) {
+void gen_random(char *s, const int len)
+{
     static const char alphanum[] =
-    "0123456789"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz";
-    
-    for (int i = 0; i < len; ++i) {
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i)
+    {
         s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
 }
-
-// TODO: Implement you helper functions here
