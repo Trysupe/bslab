52acc2243788d428291c78d58702b443e599fb24
Felix Schneider <fs@felix-schneider.org>
2020-12-28 12:31:14 +0100

    Delete Todo

--- includes/myondiskfs.h
+++ includes/myondiskfs.h
@@ -13,9 +13,10 @@
 #include "RootDir.h"
 
 /// @brief On-disk implementation of a simple file system.
-class MyOnDiskFS : public MyFS {
+class MyOnDiskFS : public MyFS
+{
 protected:
-    BlockDevice *blockDevice;  // added pointer because error from CLion
+    BlockDevice *blockDevice; // added pointer because error from CLion
     char *buffer;
     RootDir *rootDir;
     DMap *dMap;
@@ -24,11 +25,8 @@ protected:
     int openFileCount = 0;
     openFile *openFiles[NUM_OPEN_FILES];
 
-
 public:
     static MyOnDiskFS *Instance();
-
-    // TODO: [PART 1] Add attributes of your file system here
 
     MyOnDiskFS();
     ~MyOnDiskFS();
@@ -48,16 +46,14 @@ public:
     virtual int fuseRead(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo);
     virtual int fuseWrite(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo);
     virtual int fuseRelease(const char *path, struct fuse_file_info *fileInfo);
-    virtual void* fuseInit(struct fuse_conn_info *conn);
+    virtual void *fuseInit(struct fuse_conn_info *conn);
     virtual int fuseReaddir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo);
     virtual int fuseTruncate(const char *path, off_t offset, struct fuse_file_info *fileInfo);
     virtual void fuseDestroy();
 
-    // TODO: Add methods of your file system here
     int getNextFreeIndexOpenFiles();
     int readFile(int *blocks, int blockCount, int offset, size_t size, char *buf, openFile *openFile);
     int writeFile(int *blocks, int blockCount, int offset, size_t size, const char *buf, openFile *file);
-
 };
 
 #endif //MYFS_MYONDISKFS_H
