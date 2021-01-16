567313ea392284771c010e1ab393cbfb7546c729
Cédric Uden <cedric@uden.de>
2021-01-16 16:12:18 +0100

    fixed the fat

--- src/FAT.cpp
+++ src/FAT.cpp
@@ -71,51 +71,45 @@ void FAT::freeBlock(int index) {
 void FAT::persist() {
     char buffer[BLOCK_SIZE];
 
-    // get the index from the highest modified block (in global scope)
-    int index_last_modified_block_index_in_fs = modifiedBlocks[modifiedBlocksCounter - 1];
+    // go over each modified block
+    // start at the second entry since the first entry always point to the second modified...
+    // ...value and does not point anywhere if only one blockdevice is used
+    for (int j = 1; j < modifiedBlocksCounter; j++) {
 
-    // 4 because of int32_t which equals 4 bytes
-    int iterate_x_times_to_reach_last_block_index = index_last_modified_block_index_in_fs / (BLOCK_SIZE / 4);
+        // check which position we are at inside the blockdevices allocated for the FAT
+        int blockdevice_offset = (modifiedBlocks[j-1]) / (BLOCK_SIZE / 4);
 
-    // reach the last block
-    for (int i = 0; i <= iterate_x_times_to_reach_last_block_index; i++) {
+        // read the current targeted blockdata
+        device->read(FAT_OFFSET + blockdevice_offset, buffer);
 
-        // init the buffer with '-1' everywhere
-        // FF FF FF FF equals -1 in two's complement....
-        // this breaks the purpose of 'FAT_EOF' but oh well ¯\_(ツ)_/¯
-        for (int j = 0; j < BLOCK_SIZE / 4; j++) {
-            buffer[(4 * j) + 0] = 255;
-            buffer[(4 * j) + 1] = 255;
-            buffer[(4 * j) + 2] = 255;
-            buffer[(4 * j) + 3] = 255;
-        }
+        int current_block_index = modifiedBlocks[j];
 
-        // go over each modified block (while doing so redundantly)
-        // start at the second entry since the first entry always point to the second modified
-        // value and does not point anywhere if only one blockdevice is used
-        for (int j = 1; j < modifiedBlocksCounter; j++) {
 
-            int current_block_index = modifiedBlocks[j];
+        // track iterations from 0 to 512 inside the current blockdevice
+        int this_iteration = 0;
 
-            // write the updated FAT
-            for (int k = 0; k < BLOCK_SIZE; k++) {
 
-                // if the iteration matches the modified block
-                //
-                if (k == modifiedBlocks[j-1]) {
-                    // write each byte to the buffer, start from the left
-                    for (int l = 3; l >= 0; l--) {
-                        // write the next block pointer
-                        int this_byte = (current_block_index >> (8*l)) & 0xff;
-                        int buffer_offset = (k*4) + l;
-                        buffer[buffer_offset] = this_byte;
+        // write the updated FAT, keep track over the blockdevice offset
+        for (int k = 0 + ((BLOCK_SIZE / 4) * blockdevice_offset);
+        k < BLOCK_SIZE + (BLOCK_SIZE * current_block_index); k++) {
 
-                    }
+            // if the iteration matches the modified block
+            if (k == modifiedBlocks[j-1]) {
+                // write each byte to the buffer, start from the left
+                for (int l = 3; l >= 0; l--) {
+                    // write the next block pointer
+                    int this_byte = (current_block_index >> (8*l)) & 0xff;
+                    int buffer_offset = (this_iteration*4) + l;
+                    buffer[buffer_offset] = this_byte;
+
                 }
             }
+
+            this_iteration++;
         }
-        device->write(FAT_OFFSET + i, buffer);
+        device->write(FAT_OFFSET + blockdevice_offset, buffer);
     }
+
     clearModifiedBlocks();
 }
 
